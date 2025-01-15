33408e1cad0ce5d470f77544b73d56645134657f
kaith014 <kaithpaulineo@gmail.com>
2025-01-15 12:45:09 +0800

    Add files via upload

---
+++ Activity 2.cpp
@@ -0,0 +1,11 @@
+#include  <iostream>
+using namespace std;
+int main(){
+      string stringText  =  "Kaith Pauline C. Occiano";
+      string course  = "ACT-1";
+
+      cout <<  "Fullname:"  << stringText  << endl;
+      cout << "Course:"  << course << endl;
+      return 0 ;
+
+}
